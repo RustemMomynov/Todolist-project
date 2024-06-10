4c3c01ca4e62276063ad824d7608273c7e6729af
Rustem <slehder5657@gmail.com>
2024-06-10 19:16:14 +0500

    03

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 import Todolist, { TaskType } from "./layout/todolist/Todolist";
+import { v1 } from "uuid";
 
 function App() {
-  const tasks: TaskType[] = [
-    { id: 1, title: "HTML", isDone: true },
-    { id: 2, title: "CSS", isDone: true },
-    { id: 3, title: "JS/TS", isDone: false },
-    { id: 4, title: "REACT", isDone: false },
-  ];
+  let [tasks, setTasks] = useState<TaskType[]>([
+    { id: v1(), title: "HTML", isDone: true },
+    { id: v1(), title: "CSS", isDone: true },
+    { id: v1(), title: "JS/TS", isDone: false },
+    { id: v1(), title: "REACT", isDone: false },
+  ]);
+
+  const removeTask = (taskId: string) => {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  };
+
+  const addTask = (title: string) => {
+    const newTask: TaskType = {
+      id: v1(),
+      title,
+      isDone: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
 
   return (
     <div className="App">
-      <Todolist title="What to learn" tasks={tasks} />
+      <Todolist
+        title="What to learn"
+        tasks={tasks}
+        removeTask={removeTask}
+        addTask={addTask}
+      />
     </div>
   );
 }
--- src/layout/todolist/Todolist.tsx
+++ src/layout/todolist/Todolist.tsx
@@ -1,32 +1,53 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 
 export interface TodolistProps {
   title: string;
   tasks: TaskType[];
+  removeTask: (taskId: string) => void;
+  addTask: (title: string) => void;
 }
 
 export type TaskType = {
-  id: number;
+  id: string;
   title: string;
   isDone: boolean;
 };
 
 const Todolist: FC<TodolistProps> = (props) => {
+  const taskInputRef = useRef<HTMLInputElement>(null);
+
   const tasksElements = props.tasks.map((task) => {
     return (
       <li>
         <input type="checkbox" checked={task.isDone} />
         <span>{task.title}</span>
+        <button
+          onClick={() => {
+            props.removeTask(task.id);
+          }}
+        >
+          x
+        </button>
       </li>
     );
   });
+
+  const addTaskHandler = () => {
+    if (taskInputRef.current) {
+      if (taskInputRef.current.value.length < 15) {
+        props.addTask(taskInputRef.current?.value);
+      }
+
+      taskInputRef.current.value = "";
+    }
+  };
 
   return (
     <div className="todolist">
       <h3>{props.title}</h3>
       <div>
-        <input />
-        <button>+</button>
+        <input ref={taskInputRef} />
+        <button onClick={addTaskHandler}>+</button>
       </div>
       <ul>{tasksElements}</ul>
       <div>
